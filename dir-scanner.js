0fda5a4f94e9b6beade190ad041d0fe06c36788b
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-18 11:21:11 +0100

    focused watch tasks to only watch the required dir

--- dir-scanner.js
+++ dir-scanner.js
@@ -1,12 +1,5 @@
 import fs from 'fs';
 
-// fs.readdir('./PARTY_GIFS/', (err, files) => {
-//   if (err) throw err;
-//   files.forEach( file => console.log(file));
-// });
-// because trying to do something with files here won't work because
-// the callback hasn't fired yet.
-
 function getFilenames() {
   return new Promise((resolve, reject) => {
     fs.readdir('./src/assets/PARTY_GIFS/', (err, files) => {
